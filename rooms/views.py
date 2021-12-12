d8c917f778bd42b398feaf4e5953e7a5373d4a52
DS Hwang <hds2g22@gmail.com>
2021-12-13 06:11:46 +0900

    Room detail PUT

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,6 @@
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.generics import APIView, RetrieveAPIView
+from rest_framework import status
 from .models import Room
 from .serializers import ReadRoomSerializer, WriteRoomSerializer
 
@@ -24,16 +24,42 @@ class RoomsView(APIView):
 
 
 class RoomView(APIView):
-    def get(self, request, pk):
+    def get_room(self, pk):
         try:
             room = Room.objects.get(pk=pk)
+            return room
+        except Room.DoesNotExist:
+            return None
+
+    def get(self, request, pk):
+        room = self.get_room(pk)
+        if room is not None:
             serializer = ReadRoomSerializer(room).data
             return Response(serializer)
-        except Room.DoesNotExist:
+        else:
             return Response(status=status.HTTP_404_NOT_FOUND)
 
-    def put(self, request):
-        pass
+    def put(self, request, pk):
+        room = self.get_room(pk)
+        if room is not None:
+            if room.user != request.user:
+                return Response(status=status.HTTP_403_FORBIDDEN)
+            serializer = WriteRoomSerializer(room, data=request.data, partial=True)
+            if serializer.is_valid():
+                room = serializer.save()
+                return Response(ReadRoomSerializer(room).data)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response()
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    def delete(self, request):
-        pass
+    def delete(self, request, pk):
+        room = self.get_room(pk)
+        if room is not None:
+            if room.user != request.user:
+                return Response(status=status.HTTP_403_FORBIDDEN)
+            room.delete()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_404_NOT_FOUND)
