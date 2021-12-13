5beb8d7884d51a11b2538e25ce56dccb103841ad
DS Hwang <hds2g22@gmail.com>
2021-12-14 07:02:55 +0900

    Magic + FavsView

--- users/views.py
+++ users/views.py
@@ -3,11 +3,13 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework import status
+from rooms.serializers import RoomSerializer
 from .models import User
-from .serializers import ReadUserSerializer
+from .serializers import ReadUserSerializer, WriteUserSerializer
 
 
 class MeView(APIView):
+
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
@@ -29,3 +31,16 @@ def user_detail(request, pk):
         return Response(ReadUserSerializer(user).data)
     except User.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+class FavsView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = RoomSerializer(user.favs.all(), many=True).data
+        return Response(serializer)
+
+    def put(self, request):
+        pass
